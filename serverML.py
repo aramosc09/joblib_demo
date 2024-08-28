0cddcfadbe2da508df32a708bfc67eb34d527bf9
Alejandro Ramos <aramosc03@gmail.com>
2024-08-27 19:47:58 -0600

    manejo automatico del dir7

--- serverML.py
+++ serverML.py
@@ -19,15 +19,14 @@ def predictjson():
     # Process data
     data = request.json
     # print(data)
-    # inputData = np.array([
-    #     data['alcohol'],
-    #     data['volatile acidity'],
-    #     data['pH']
-    # ])
-    inputData = [data['alcohol'], data['volatile acidity'], data['pH']]
+    inputData = np.array([
+        data['alcohol'],
+        data['volatile acidity'],
+        data['pH']
+    ])
 
     # Predict using input & model
-    result = dt.predict(inputData[0])
+    result = dt.predict([inputData.reshape(1, -1)])
 
     # Send answer
     return jsonify({'Prediction': str(result[0])})
