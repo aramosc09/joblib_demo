1949ec48e35b47d8edc4ee38a449833f8b039990
Alejandro Ramos <aramosc03@gmail.com>
2024-08-27 18:52:59 -0600

    servidor

--- serverML.py
+++ serverML.py
@@ -13,3 +13,26 @@ server = Flask(__name__)
 
 # Define a route to send json data
 server.route('/predictjson',methods=['POST'])
+
+# Define a route to send JSON data
+
+@server.route('predictjson',method=['POST'])
+def predictjson():
+    # Process data
+    data = request.json
+    # print(data)
+    # inputData = np.array([
+    #     data['alcohol'],
+    #     data['volatile acidity'],
+    #     data['pH']
+    # ])
+    inputData = [data['alcohol'], data['volatile acidity'], data['pH']]
+
+    # Predict using input & model
+    result = dt.predict(inputData[0])
+
+    # Send answer
+    return jsonify({'Prediction': str(result[0])})
+
+if __name__ == '__main__':
+    server.run(debug=False,host='0.0.0.0',port=8080)
